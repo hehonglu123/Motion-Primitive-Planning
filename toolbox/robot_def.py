f83d831b543906dccb4b9173b1ea9f9d0c758d25
hehonglu123 <hehonglu123@126.com>
2021-08-26 15:58:43 -0400

    jdot

--- toolbox/robot_def.py
+++ toolbox/robot_def.py
@@ -39,12 +39,80 @@ ABB_def=Robot(H,P,joint_type,joint_lower_limit = lowerer_limit, joint_upper_limi
 def jacobian(q):
 	return robotjacobian(ABB_def,q)
 def fwd(q):
-    return fwdkin(ABB_def,q)
+	return fwdkin(ABB_def,q)
 
 def inv(p,R=np.eye(3)):
 	pose=Transform(R,p)
 	q_all=robot6_sphericalwrist_invkin(ABB_def,pose)
 	return q_all
+def HomogTrans(q,h,p,jt):
+
+	if jt==0:
+		H=np.vstack((np.hstack((rot(h,q), p.reshape((3,1)))),np.array([0, 0, 0, 1,])))
+	else:
+		H=np.vstack((np.hstack((np.eye(3), p + np.dot(q, h))),np.array([0, 0, 0, 1,])))
+	return H
+def Hvec(h,jtype):
+
+	if jtype>0:
+		H=np.vstack((np.zeros((3,1)),h))
+	else:
+		H=np.vstack((h.reshape((3,1)),np.zeros((3,1))))
+	return H
+def phi(R,p):
+
+	Phi=np.vstack((np.hstack((R,np.zeros((3,3)))),np.hstack((-np.dot(R,hat(p)),R))))
+	return Phi
 
 
-print(jacobian(np.zeros(6)))
+def jdot(q,qdot):
+	zv=np.zeros((3,1))
+	H=np.eye(4)
+	J=[]
+	Jdot=[]
+	n=6
+	Jmat=[]
+	Jdotmat=[]
+	for i in range(n+1):
+		if i<n:
+			hi=ABB_def.H[:,i]
+			qi=q[i]
+			qdi=qdot[i]
+			ji=ABB_def.joint_type[i]
+
+		else:
+			qi=0
+			qdi=0
+			di=0
+			ji=0
+
+		Pi=ABB_def.P[:,i]
+		Hi=HomogTrans(qi,hi,Pi,ji)
+		Hn=np.dot(H,Hi)
+		H=Hn
+
+		PHI=phi(Hi[:3,:3].T,Hi[:3,-1])
+		Hveci=Hvec(hi,ji)
+		###Partial Jacobian progagation
+		if(len(J)>0):
+			Jn=np.hstack((np.dot(PHI,J), Hveci))
+			temp=np.vstack((np.hstack((hat(hi), np.zeros((3,3)))),np.hstack((np.zeros((3,3)),hat(hi)))))
+			Jdotn=-np.dot(qdi,np.dot(temp,Jn)) + np.dot(PHI,np.hstack((Jdot, np.zeros(Hveci.shape))))
+		else:
+			Jn=Hveci
+			Jdotn=np.zeros(Jn.shape)
+
+		Jmat.append(Jn) 
+		Jdotmat.append(Jdotn)
+		J=Jn
+		Jdot=Jdotn
+
+	Jmat[-1]=Jmat[-1][:,:n]
+	Jdotmat[-1]=Jdotmat[-1][:,:n]
+	return Jdotmat[-1]
+
+
+
+
+
+print(jdot(np.zeros(6),np.ones(6)))
