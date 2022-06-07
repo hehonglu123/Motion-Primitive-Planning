2c3f4d09e67e9f634f619584f0bfc633fe14d6da
hehonglu123 <hehonglu123@126.com>
2022-06-07 13:53:10 -0400

    realr obto

---
+++ ILC/ilc_exe_realrobot.py
@@ -0,0 +1,149 @@
+########
+# This module utilized https://github.com/johnwason/abb_motion_program_exec
+# and send whatever the motion primitives that algorithms generate
+# to RobotStudio
+########
+
+import numpy as np
+from general_robotics_toolbox import *
+from pandas import read_csv
+import sys
+from io import StringIO
+
+# sys.path.append('../abb_motion_program_exec')
+from abb_motion_program_exec_client import *
+from ilc_toolbox import *
+sys.path.append('../toolbox')
+from robots_def import *
+from error_check import *
+from MotionSend import *
+from lambda_calc import *
+from blending import *
+
+def main():
+	# data_dir="fitting_output_new/python_qp_movel/"
+	dataset='from_NX/'
+	data_dir="../data/"+dataset
+	fitting_output="../data/"+dataset+'baseline/100L/'
+
+
+	curve_js=read_csv(data_dir+'Curve_js.csv',header=None).values
+	curve = read_csv(data_dir+"Curve_in_base_frame.csv",header=None).values
+
+
+	max_error_threshold=0.1
+	robot=abb6640(d=50)
+
+	s = speeddata(800,9999999,9999999,999999)
+	z = z10
+
+
+	curve_fit_js=read_csv(fitting_output+'curve_fit_js.csv',header=None).values
+
+	ms = MotionSend()
+	breakpoints,primitives,p_bp,q_bp=ms.extract_data_from_cmd(fitting_output+'command.csv')
+
+	###extension
+	primitives,p_bp,q_bp=ms.extend(robot,q_bp,primitives,breakpoints,p_bp)
+
+	###ilc toolbox def
+	ilc=ilc_toolbox(robot,primitives)
+
+	###TODO: extension fix start point, moveC support
+	max_error=999
+	inserted_points=[]
+	i=0
+	iteration=20
+	while max_error>max_error_threshold:
+		i+=1
+		ms = MotionSend()
+		###execute,curve_fit_js only used for orientation
+		logged_data=ms.exec_motions(robot,primitives,breakpoints,p_bp,q_bp,s,z)
+
+		StringData=StringIO(logged_data)
+		df = read_csv(StringData, sep =",")
+		##############################data analysis#####################################
+		lam, curve_exe, curve_exe_R,curve_exe_js, speed, timestamp=ms.logged_data_analysis(robot,df)
+		#############################chop extension off##################################
+		start_idx=np.argmin(np.linalg.norm(curve[0,:3]-curve_exe,axis=1))
+		end_idx=np.argmin(np.linalg.norm(curve[-1,:3]-curve_exe,axis=1))
+
+		#make sure extension doesn't introduce error
+		if np.linalg.norm(curve_exe[start_idx]-curve[0,:3])>0.5:
+			start_idx+=1
+		if np.linalg.norm(curve_exe[end_idx]-curve[-1,:3])>0.5:
+			end_idx-=1
+
+		curve_exe=curve_exe[start_idx:end_idx+1]
+		curve_exe_js=curve_exe_js[start_idx:end_idx+1]
+		curve_exe_R=curve_exe_R[start_idx:end_idx+1]
+		speed=speed[start_idx:end_idx+1]
+		speed=replace_outliers(np.array(speed))
+		lam=calc_lam_cs(curve_exe)
+
+		##############################calcualte error########################################
+		error,angle_error=calc_all_error_w_normal(curve_exe,curve[:,:3],curve_exe_R[:,:,-1],curve[:,3:])
+		max_error=max(error)
+		print(max_error)
+		max_angle_error=max(angle_error)
+		max_error_idx=np.argmax(error)#index of exe curve with max error
+		_,max_error_curve_idx=calc_error(curve_exe[max_error_idx],curve[:,:3])  # index of original curve closest to max error point
+		exe_error_vector=(curve[max_error_curve_idx,:3]-curve_exe[max_error_idx])           # shift vector
+		
+		##############################plot error#####################################
+		if i>iteration:
+			fig, ax1 = plt.subplots()
+			ax2 = ax1.twinx()
+			ax1.plot(lam, speed, 'g-', label='Speed')
+			ax2.plot(lam, error, 'b-',label='Error')
+			ax2.plot(lam, np.degrees(angle_error), 'y-',label='Normal Error')
+
+			ax1.set_xlabel('lambda (mm)')
+			ax1.set_ylabel('Speed/lamdot (mm/s)', color='g')
+			ax2.set_ylabel('Error/Normal Error (mm/deg)', color='b')
+			plt.title("Speed and Error Plot")
+			ax1.legend(loc=0)
+
+			ax2.legend(loc=0)
+
+			plt.legend()
+			###########################find closest bp####################################
+			bp_idx=np.absolute(breakpoints-max_error_curve_idx).argmin()
+			###########################plot for verification###################################
+			plt.figure()
+			ax = plt.axes(projection='3d')
+			ax.plot3D(curve[:,0], curve[:,1], curve[:,2], c='gray',label='original')
+			ax.plot3D(curve_exe[:,0], curve_exe[:,1], curve_exe[:,2], c='red',label='execution')
+			p_bp_np=np.array(p_bp[1:-1])      ###np version, avoid first and last extended points
+			ax.scatter3D(p_bp_np[:,0], p_bp_np[:,1], p_bp_np[:,2], c=p_bp_np[:,2], cmap='Greens',label='breakpoints')
+			ax.scatter(curve_exe[max_error_idx,0], curve_exe[max_error_idx,1], curve_exe[max_error_idx,2],c='orange',label='worst case')
+		
+		
+		##########################################calculate gradient######################################
+		######gradient calculation related to nearest 3 points from primitive blended trajectory, not actual one
+		###restore trajectory from primitives
+		curve_interp, curve_R_interp, curve_js_interp, breakpoints_blended=form_traj_from_bp(q_bp,primitives,robot)
+
+		curve_js_blended,curve_blended,curve_R_blended=blend_js_from_primitive(curve_interp, curve_js_interp, breakpoints_blended, primitives,robot,zone=10)
+
+		###get closest to worst case point on blended trajectory
+		_,max_error_curve_blended_idx=calc_error(curve_exe[max_error_idx],curve_blended)
+		curve_blended_point=copy.deepcopy(curve_blended[max_error_curve_blended_idx])
+
+		###############get numerical gradient#####
+		###find closest 3 breakpoints
+		order=np.argsort(np.abs(breakpoints_blended-max_error_curve_blended_idx))
+		breakpoint_interp_2tweak_indices=order[:3]
+
+		de_dp=ilc.get_gradient_from_model_xyz(q_bp,p_bp,breakpoints_blended,curve_blended,max_error_curve_blended_idx,robot.fwd(curve_exe_js[max_error_idx]),curve[max_error_curve_idx,:3],breakpoint_interp_2tweak_indices)
+		p_bp, q_bp=ilc.update_bp_xyz(p_bp,q_bp,de_dp,max_error,breakpoint_interp_2tweak_indices)
+
+		if i>iteration:
+			for m in breakpoint_interp_2tweak_indices:
+				ax.scatter(p_bp[m][0], p_bp[m][1], p_bp[m][2],c='blue',label='adjusted breakpoints')
+			plt.legend()
+			plt.show()
+
+
+if __name__ == "__main__":
+	main()
--- ILC/ilc_toolbox.py
+++ ILC/ilc_toolbox.py
@@ -33,65 +33,15 @@ class ilc_toolbox(object):
 					,car2js(self.robot,curve_fit_js[breakpoints[i]],points_list[i][0],self.robot.fwd(curve_fit_js[breakpoints[i]]).R)[0]])
 		return q_bp
 
-	def get_gradient_from_model_6(self,q_bp,breakpoints_blended,curve_blended,curve_R_blended,max_error_curve_blended_idx,worst_point_pose,closest_p,closest_N):
-		###q_bp:				joint configs at breakpoints
-		###breakpoints_blended:	breakpoints of blended trajectory
-		###curve_blended:		blended trajectory
-		###curve_R_blended:		blended trajectory
-		###max_error_curve_blended_idx:	p', closest point to worst case error on blended trajectory
-		###worst_point_pose:	execution curve with worst case pose
-		###closest_p:			closest point on original curve
-		###closest_N:			closest point on original curve
-
-		de_dp=[]    #de_dp1q1,de_dp1q2,...,de_dp3q6
-		de_ori_dp=[]
-		delta=0.01 	#rad
-		###find closest 3 breakpoints
-		order=np.argsort(np.abs(breakpoints_blended-max_error_curve_blended_idx))
-		breakpoint_interp_2tweak_indices=order[:3]
-
-		###len(primitives)==len(breakpoints)==len(breakpoints_blended)==len(points_list)
-		for m in breakpoint_interp_2tweak_indices:  #3 breakpoints
-			for n in range(6): #6DOF, q1~q6
-				q_bp_temp=copy.deepcopy(q_bp)
-				q_bp_temp[m]+=delta
-				#restore new trajectory
-				curve_interp_temp, curve_R_interp_temp, curve_js_interp_temp, breakpoints_blended_temp=form_traj_from_bp(q_bp_temp,primitives,robot)
-				curve_js_blended_temp,curve_blended_temp,curve_R_blended_temp=blend_js_from_primitive(curve_interp_temp, curve_js_interp_temp, breakpoints_blended_temp, primitives,robot,zone=10)
-				
-
-				worst_case_point_shift=curve_blended_temp[max_error_curve_blended_idx]-curve_blended[max_error_curve_blended_idx]
-				de=np.linalg.norm(worst_point_pose.p+worst_case_point_shift-closest_p)
-				worst_case_R_shift=curve_R_blended_temp[max_error_curve_blended_idx]@curve_R_blended[max_error_curve_blended_idx].T
-				de_ori=get_angle(worst_case_R_shift@worst_point_pose.R,closest_N)
-
-				de_dp.append(de/delta)
-				de_ori_dp.append(de_ori/delta)
-
-		de_dp=np.reshape(de_dp,(-1,1))
-		de_ori_dp=np.reshape(de_ori_dp,(-1,1))
-
-		return de_dp, de_ori_dp
-
-	def update_bp_6(self,points_list,q_list,q_bp,de_dp,de_ori_dp,max_error,max_ori_error,breakpoint_interp_2tweak_indices,alpha1=0.5,alpha2=0.1):
-		bp_q_adjustment=-alpha1*np.linalg.pinv(de_dp)*max_error-alpha2*np.linalg.pinv(de_ori_dp)*max_ori_error
-		for i in range(len(breakpoint_interp_2tweak_indices)):  #3 breakpoints
-			q_bp[breakpoint_interp_2tweak_indices[i]]+=bp_q_adjustment[0][6*i:6*(i+1)]
-			pose_temp=self.robot.fwd(q_bp[breakpoint_interp_2tweak_indices[i]])
-			points_list[breakpoint_interp_2tweak_indices[i]]=pose_temp.p
-			q_list[breakpoint_interp_2tweak_indices[i]]=R2q(pose_temp.R)
-
-		return points_list, q_list
-
-
+	
 	def get_gradient_from_model_xyz(self,q_bp,p_bp,breakpoints_blended,curve_blended,max_error_curve_blended_idx,worst_point_pose,closest_p,breakpoint_interp_2tweak_indices):
-		###q_bp:				joint configs at breakpoints
-		###p_bp:				xyz configs at breakpoints
-		###breakpoints_blended:	breakpoints of blended trajectory
-		###curve_blended:		blended trajectory
-		###max_error_curve_blended_idx:	p', closest point to worst case error on blended trajectory
-		###worst_point_pose:	execution curve with worst case pose
-		###closest_p:			closest point on original curve
+		###q_bp:								joint configs at breakpoints
+		###p_bp:								xyz configs at breakpoints
+		###breakpoints_blended:					breakpoints of blended trajectory
+		###curve_blended:						blended trajectory
+		###max_error_curve_blended_idx:	p', 	closest point to worst case error on blended trajectory
+		###worst_point_pose:					execution curve with worst case pose
+		###closest_p:							closest point on original curve
 		###breakpoint_interp_2tweak_indices:	closest N breakpoints
 
 		###TODO:ADD MOVEC SUPPORT
@@ -121,6 +71,7 @@ class ilc_toolbox(object):
 
 				curve_blended_new[start_idx:end_idx]=curve_blended_temp[start_idx-breakpoints_blended[short_version[0]]:-1-(breakpoints_blended[short_version[-1]]-end_idx)]
 
+				###calculate relative gradient
 				worst_case_point_shift=curve_blended_new[max_error_curve_blended_idx]-curve_blended[max_error_curve_blended_idx]
 				###get new error
 				de=np.linalg.norm(worst_point_pose.p+worst_case_point_shift-closest_p)-np.linalg.norm(worst_point_pose.p-closest_p)
@@ -132,6 +83,12 @@ class ilc_toolbox(object):
 		return de_dp
 
 	def update_bp_xyz(self,p_bp,q_bp,de_dp,max_error,breakpoint_interp_2tweak_indices,alpha=0.5):
+		###p_bp:								xyz of breakpoints
+		###q_bp:								joint configs of breakpoints
+		###de_dp；								gradient of error and each breakpoints
+		###max_error:							worst case error value
+		###breakpoint_interp_2tweak_indices:	closest N breakpoints
+		###alpha:								stepsize
 
 		point_adjustment=-alpha*np.linalg.pinv(de_dp)*max_error
 
@@ -142,53 +99,71 @@ class ilc_toolbox(object):
 
 		return p_bp, q_bp
 
-	def get_gradient_from_model_ori(self,q_bp,R_bp,breakpoints_blended,curve_R_blended,max_error_curve_blended_idx,worst_point_pose,closest_R):
-		###q_bp:				joint configs at breakpoints
-		###R_bp:				ori configs at breakpoints
-		###breakpoints_blended:	breakpoints of blended trajectory
-		###curve_R_blended:		blended trajectory R
-		###curve_R_blended:		blended trajectory
-		###max_error_curve_blended_idx:	p', closest point to worst case error on blended trajectory
-		###worst_point_pose:	execution curve with worst case pose
-		###closest_R:			closest point on original curve
+
+	def get_gradient_from_model_6j(self,q_bp,breakpoints_blended,curve_blended,curve_R_blended,max_error_curve_blended_idx,worst_point_pose,closest_p,closest_N,breakpoint_interp_2tweak_indices):
+		###q_bp:								joint configs at breakpoints
+		###breakpoints_blended:					breakpoints of blended trajectory
+		###curve_blended:						blended trajectory
+		###curve_R_blended:						blended trajectory
+		###max_error_curve_blended_idx:	p', 	closest point to worst case error on blended trajectory
+		###worst_point_pose:					execution curve with worst case pose
+		###closest_p:							closest point on original curve
+		###closest_N:							closest point on original curve, its normal
+		###breakpoint_interp_2tweak_indices:	closest N breakpoints
 
 		de_dp=[]    #de_dp1q1,de_dp1q2,...,de_dp3q6
 		de_ori_dp=[]
 		delta=0.01 	#rad
-		###find closest 3 breakpoints
-		order=np.argsort(np.abs(breakpoints_blended-max_error_curve_blended_idx))
-		breakpoint_interp_2tweak_indices=order[:3]
+
 
 		###len(primitives)==len(breakpoints)==len(breakpoints_blended)==len(points_list)
 		for m in breakpoint_interp_2tweak_indices:  #3 breakpoints
-			for n in range(3): #3DOF, xyz
+			for n in range(6): #6DOF, q1~q6
 				q_bp_temp=copy.deepcopy(q_bp)
-				p_bp_temp=copy.deepcopy(p_bp)
-				p_bp_temp[m][n]+=delta
-				q_bp_temp[m]=car2js(robot,q_bp_temp[m],np.array(q_bp_temp[m]),robot.fwd(q_bp_temp[m]).R)[0]
-
-				#restore new trajectory
-				curve_interp_temp, curve_R_interp_temp, curve_js_interp_temp, breakpoints_blended_temp=form_traj_from_bp(q_bp_temp,primitives,robot)
-				curve_js_blended_temp,curve_blended_temp,curve_R_blended_temp=blend_js_from_primitive(curve_interp_temp, curve_js_interp_temp, breakpoints_blended_temp, primitives,robot,zone=10)
+				q_bp_temp[m][0][n]+=delta		###ADD MOVEC SUPPORT
 				
+				#restore new trajectory, only for adjusted breakpoint, 1-bp change requires traj interp from 5 bp
+				short_version=range(max(m-2,0),min(m+2,len(breakpoints_blended)-1))
+				curve_interp_temp, curve_R_interp_temp, curve_js_interp_temp, breakpoints_blended_temp=form_traj_from_bp(q_bp_temp[short_version],[self.primitives[i] for i in short_version],self.robot)
 
-				worst_case_point_shift=curve_blended_temp[max_error_curve_blended_idx]-curve_blended[max_error_curve_blended_idx]
-				de=np.linalg.norm(worst_point_pose.p+worst_case_point_shift-closest_p)
+				curve_js_blended_temp,curve_blended_temp,curve_R_blended_temp=blend_js_from_primitive(curve_interp_temp, curve_js_interp_temp, breakpoints_blended_temp, [self.primitives[i] for i in short_version],self.robot,zone=10)
+				
+				curve_blended_new=copy.deepcopy(curve_blended)
+				start_idx=int((breakpoints_blended[short_version[0]]+breakpoints_blended[short_version[1]])/2)
+				end_idx=int((breakpoints_blended[short_version[-1]]+breakpoints_blended[short_version[-2]])/2)
+
+				curve_blended_new[start_idx:end_idx]=curve_blended_temp[start_idx-breakpoints_blended[short_version[0]]:-1-(breakpoints_blended[short_version[-1]]-end_idx)]
+
+				###calculate relative gradient, xyz
+				worst_case_point_shift=curve_blended_new[max_error_curve_blended_idx]-curve_blended[max_error_curve_blended_idx]
+				###get new error
+				de=np.linalg.norm(worst_point_pose.p+worst_case_point_shift-closest_p)-np.linalg.norm(worst_point_pose.p-closest_p)
+
+				###calculate relative gradient, ori
+				worst_case_R_shift=curve_R_blended_temp[max_error_curve_blended_idx]@curve_R_blended[max_error_curve_blended_idx].T
+				de_ori=get_angle(worst_case_R_shift@worst_point_pose.R,closest_N)-get_angle(worst_point_pose.R,closest_N)
 
 				de_dp.append(de/delta)
+				de_ori_dp.append(de_ori/delta)
 
 		de_dp=np.reshape(de_dp,(-1,1))
+		de_ori_dp=np.reshape(de_ori_dp,(-1,1))
 
-		return de_dp
+		return de_dp, de_ori_dp
 
+	def update_bp_6j(self,p_bp,q_bp,de_dp,de_ori_dp,max_error,max_ori_error,breakpoint_interp_2tweak_indices,alpha1=0.5,alpha2=0.1):
+		###p_bp:								xyz of breakpoints
+		###q_bp:								joint configs of breakpoints
+		###de_dp:								gradient of error and each breakpoints
+		###de_ori_dp:							gradient of orientation error and each breakpoints
+		###max_error:							worst case error value
+		###max_ori_error:						worst case error angle
+		###breakpoint_interp_2tweak_indices:	closest N breakpoints
+		###alpha1, alpha2:						stepsize for xyz and orientation
 
-	def update_bp_ori(self,points_list,q_list,q_bp,de_dp,de_ori_dp,max_error,max_ori_error,breakpoint_interp_2tweak_indices,alpha1=0.5,alpha2=0.1):
 		bp_q_adjustment=-alpha1*np.linalg.pinv(de_dp)*max_error-alpha2*np.linalg.pinv(de_ori_dp)*max_ori_error
 		for i in range(len(breakpoint_interp_2tweak_indices)):  #3 breakpoints
 			q_bp[breakpoint_interp_2tweak_indices[i]]+=bp_q_adjustment[0][6*i:6*(i+1)]
-			pose_temp=self.robot.fwd(q_bp[breakpoint_interp_2tweak_indices[i]])
-			points_list[breakpoint_interp_2tweak_indices[i]]=pose_temp.p
-			q_list[breakpoint_interp_2tweak_indices[i]]=R2q(pose_temp.R)
+			p_bp[breakpoint_interp_2tweak_indices[i]]=self.robot.fwd(q_bp[breakpoint_interp_2tweak_indices[i]]).p
 
-		return points_list, q_list
-
+		return p_bp, q_bp
