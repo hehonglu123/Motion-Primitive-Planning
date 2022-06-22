8e8b47245a1098ca61e0c6f014bd1a8a6225bc0f
hehonglu123 <hehonglu123@126.com>
2022-06-22 15:20:33 -0400

    max gradient real robot

--- ILC/max_gradient_realrobot.py
+++ ILC/max_gradient_realrobot.py
@@ -36,7 +36,8 @@ def main():
 	max_error_threshold=0.5
 	robot=abb6640(d=50)
 
-	s = speeddata(1100,9999999,9999999,999999)
+	v=250
+	s = speeddata(v,9999999,9999999,999999)
 	z = z10
 
 
@@ -45,7 +46,7 @@ def main():
 	# ###extension
 	# primitives,p_bp,q_bp=ms.extend(robot,q_bp,primitives,breakpoints,p_bp)
 	###########################################get cmd from simulation improved cmd################################
-	breakpoints,primitives,p_bp,q_bp=ms.extract_data_from_cmd('recorded_data/curve2_1100/command.csv')
+	breakpoints,primitives,p_bp,q_bp=ms.extract_data_from_cmd('max_gradient/curve1_250_100L_multipeak/command.csv')
 
 	###ilc toolbox def
 	ilc=ilc_toolbox(robot,primitives)
@@ -53,10 +54,10 @@ def main():
 	###TODO: extension fix start point, moveC support
 	max_error=999
 	inserted_points=[]
-	i=0
-	iteration=0
-	while max_error>max_error_threshold:
-		i+=1
+
+	iteration=5
+	for i in range(iteration):
+
 		ms = MotionSend(url='http://192.168.55.1:80')
 		###5 run execute
 		curve_exe_all=[]
@@ -89,40 +90,34 @@ def main():
 		error,angle_error=calc_all_error_w_normal(curve_exe,curve[:,:3],curve_exe_R[:,:,-1],curve[:,3:])
 
 
-		max_error=max(error)
-		print(max_error)
-		max_angle_error=max(angle_error)
-		max_error_idx=np.argmax(error)#index of exe curve with max error
-		_,max_error_curve_idx=calc_error(curve_exe[max_error_idx],curve[:,:3])  # index of original curve closest to max error point
-		exe_error_vector=(curve[max_error_curve_idx,:3]-curve_exe[max_error_idx])           # shift vector
+		##############################calcualte error########################################
+		error,angle_error=calc_all_error_w_normal(curve_exe,curve[:,:3],curve_exe_R[:,:,-1],curve[:,3:])
+		print(max(error))
+		#############################error peak detection###############################
+		peaks,_=find_peaks(error,height=multi_peak_threshold,prominence=0.05,distance=20/(lam[int(len(lam)/2)]-lam[int(len(lam)/2)-1]))		###only push down peaks higher than height, distance between each peak is 20mm, threshold to filter noisy peaks
+		
+		if len(peaks)==0 or np.argmax(error) not in peaks:
+			peaks=np.append(peaks,np.argmax(error))
 		
 		##############################plot error#####################################
-		if i>iteration:
-			fig, ax1 = plt.subplots()
-			ax2 = ax1.twinx()
-			ax1.plot(lam, speed, 'g-', label='Speed')
-			ax2.plot(lam, error, 'b-',label='Error')
-			ax2.plot(lam, np.degrees(angle_error), 'y-',label='Normal Error')
+		fig, ax1 = plt.subplots()
+		ax2 = ax1.twinx()
+		ax1.plot(lam, speed, 'g-', label='Speed')
+		ax2.plot(lam, error, 'b-',label='Error')
+		ax2.plot(lam, np.degrees(angle_error), 'y-',label='Normal Error')
 
-			ax1.set_xlabel('lambda (mm)')
-			ax1.set_ylabel('Speed/lamdot (mm/s)', color='g')
-			ax2.set_ylabel('Error/Normal Error (mm/deg)', color='b')
-			plt.title("Speed and Error Plot")
-			ax1.legend(loc=0)
+		ax1.set_xlabel('lambda (mm)')
+		ax1.set_ylabel('Speed/lamdot (mm/s)', color='g')
+		ax2.set_ylabel('Error/Normal Error (mm/deg)', color='b')
+		plt.title("Speed and Error Plot")
+		ax1.legend(loc=0)
 
-			ax2.legend(loc=0)
+		ax2.legend(loc=0)
 
-			plt.legend()
-			###########################find closest bp####################################
-			bp_idx=np.absolute(breakpoints-max_error_curve_idx).argmin()
-			###########################plot for verification###################################
-			plt.figure()
-			ax = plt.axes(projection='3d')
-			ax.plot3D(curve[:,0], curve[:,1], curve[:,2], c='gray',label='original')
-			ax.plot3D(curve_exe[:,0], curve_exe[:,1], curve_exe[:,2], c='red',label='execution')
-			p_bp_np=np.array(p_bp[1:-1])      ###np version, avoid first and last extended points
-			ax.scatter3D(p_bp_np[:,0,0], p_bp_np[:,0,1], p_bp_np[:,0,2], c=p_bp_np[:,0,2], cmap='Greens',label='breakpoints')
-			ax.scatter(curve_exe[max_error_idx,0], curve_exe[max_error_idx,1], curve_exe[max_error_idx,2],c='orange',label='worst case')
+		plt.legend()
+		# plt.savefig('iteration_ '+str(i))
+		# plt.clf()
+		plt.show()
 		
 		
 		##########################################calculate gradient######################################
@@ -132,17 +127,21 @@ def main():
 
 		curve_js_blended,curve_blended,curve_R_blended=blend_js_from_primitive(curve_interp, curve_js_interp, breakpoints_blended, primitives,robot,zone=10)
 
-		###get closest to worst case point on blended trajectory
-		_,max_error_curve_blended_idx=calc_error(curve_exe[max_error_idx],curve_blended)
-		curve_blended_point=copy.deepcopy(curve_blended[max_error_curve_blended_idx])
+		for peak in peaks:
+			######gradient calculation related to nearest 3 points from primitive blended trajectory, not actual one
+			_,peak_error_curve_idx=calc_error(curve_exe[peak],curve[:,:3])  # index of original curve closest to max error point
 
-		###############get numerical gradient#####
-		###find closest 3 breakpoints
-		order=np.argsort(np.abs(breakpoints_blended-max_error_curve_blended_idx))
-		breakpoint_interp_2tweak_indices=order[:3]
+			###get closest to worst case point on blended trajectory
+			_,peak_error_curve_blended_idx=calc_error(curve_exe[peak],curve_blended)
+			curve_blended_point=copy.deepcopy(curve_blended[peak_error_curve_blended_idx])
 
-		de_dp=ilc.get_gradient_from_model_xyz(q_bp,p_bp,breakpoints_blended,curve_blended,max_error_curve_blended_idx,robot.fwd(curve_exe_js[max_error_idx]),curve[max_error_curve_idx,:3],breakpoint_interp_2tweak_indices)
-		p_bp, q_bp=ilc.update_bp_xyz(p_bp,q_bp,de_dp,max_error,breakpoint_interp_2tweak_indices)
+			###############get numerical gradient#####
+			###find closest 3 breakpoints
+			order=np.argsort(np.abs(breakpoints_blended-peak_error_curve_blended_idx))
+			breakpoint_interp_2tweak_indices=order[:3]
+
+			de_dp=ilc.get_gradient_from_model_xyz(q_bp,p_bp,breakpoints_blended,curve_blended,peak_error_curve_blended_idx,robot.fwd(curve_exe_js[peak]),curve[peak_error_curve_idx,:3],breakpoint_interp_2tweak_indices)
+			p_bp, q_bp=ilc.update_bp_xyz(p_bp,q_bp,de_dp,error[peak],breakpoint_interp_2tweak_indices)
 
 		if i>iteration:
 			for m in breakpoint_interp_2tweak_indices:
