d13a2ab139b312c7dae2aafac90bb674d7058b3e
Yunshi Wen <randywen0612@gmail.com>
2022-07-01 14:15:10 -0400

    RL growth update.

--- data/wood/traj_gen.py
+++ data/wood/traj_gen.py
@@ -49,65 +49,70 @@ def find_next_point(t,p,step_size):
     return t_next, p_next, normal_next
 
 
+def main():
 
-t = np.linspace(0,1000,1000)
-curve=find_point(t)
-curve_normal=find_normal(curve)
+    t = np.linspace(0,1000,1000)
+    curve=find_point(t)
+    curve_normal=find_normal(curve)
 
-##############3D plots####################
-xsurf = np.linspace(0,1000,100)
-ysurf = np.linspace(- R,R,100)
-zsurf = np.zeros((len(xsurf),len(ysurf)))
-for i in range(len(ysurf)):
-    zsurf[:,i] = (R ** 2 - ysurf[i] ** 2) * H / R ** 2
+    ##############3D plots####################
+    xsurf = np.linspace(0,1000,100)
+    ysurf = np.linspace(- R,R,100)
+    zsurf = np.zeros((len(xsurf),len(ysurf)))
+    for i in range(len(ysurf)):
+        zsurf[:,i] = (R ** 2 - ysurf[i] ** 2) * H / R ** 2
 
-fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-ax.plot_surface(xsurf, ysurf, zsurf, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
-ax.plot3D(curve[:,0],curve[:,1],curve[:,2],'r.-')
-ax.quiver(curve[:,0],curve[:,1],curve[:,2],curve_normal[:,0],curve_normal[:,1],curve_normal[:,2],length=1, normalize=True)
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    ax.plot_surface(xsurf, ysurf, zsurf, cmap=cm.coolwarm,
+                           linewidth=0, antialiased=False)
+    ax.plot3D(curve[:,0],curve[:,1],curve[:,2],'r.-')
+    ax.quiver(curve[:,0],curve[:,1],curve[:,2],curve_normal[:,0],curve_normal[:,1],curve_normal[:,2],length=1, normalize=True)
 
-plt.show()
+    plt.show()
 
-###################check curve normal##############################
-# curve_tan=np.gradient(curve,axis=0)
-# print(curve_tan)
-# for i in range(len(curve_tan)):
-#     print(curve_tan[i]@curve_normal[i])
-# plt.plot(np.diag(np.inner(curve_tan,curve_normal)))
-# plt.show()
-# lam=calc_lam_cs(curve)
-# diff=np.linalg.norm(np.diff(curve,axis=0),axis=1)
-# plt.plot(diff)
-# plt.show()
+    ###################check curve normal##############################
+    # curve_tan=np.gradient(curve,axis=0)
+    # print(curve_tan)
+    # for i in range(len(curve_tan)):
+    #     print(curve_tan[i]@curve_normal[i])
+    # plt.plot(np.diag(np.inner(curve_tan,curve_normal)))
+    # plt.show()
+    # lam=calc_lam_cs(curve)
+    # diff=np.linalg.norm(np.diff(curve,axis=0),axis=1)
+    # plt.plot(diff)
+    # plt.show()
 
-####################generate equally spaced points##########################
-num_points=50000
-lam=calc_lam_cs(curve)
-lam=np.linspace(0,lam[-1],num_points)
-curve_act=[curve[0]]
-curve_normal_act=[curve_normal[0]]
-t_act=[0]
-lam_act=np.linspace(0,lam[-1],num_points)
-for i in range(1,num_points):
-    t_next, p_next, normal_next=find_next_point(t_act[-1],curve_act[-1],lam[i]-lam[i-1])
-    curve_act.append(p_next.flatten())
-    curve_normal_act.append(normal_next.flatten())
-    t_act.append(t_next)
+    ####################generate equally spaced points##########################
+    num_points=50000
+    lam=calc_lam_cs(curve)
+    lam=np.linspace(0,lam[-1],num_points)
+    curve_act=[curve[0]]
+    curve_normal_act=[curve_normal[0]]
+    t_act=[0]
+    lam_act=np.linspace(0,lam[-1],num_points)
+    for i in range(1,num_points):
+        t_next, p_next, normal_next=find_next_point(t_act[-1],curve_act[-1],lam[i]-lam[i-1])
+        curve_act.append(p_next.flatten())
+        curve_normal_act.append(normal_next.flatten())
+        t_act.append(t_next)
 
-curve_act=np.array(curve_act)
-curve_normal_act=np.array(curve_normal_act)
+    curve_act=np.array(curve_act)
+    curve_normal_act=np.array(curve_normal_act)
 
-DataFrame(np.hstack((curve_act,curve_normal_act))).to_csv('Curve_dense.csv',header=False,index=False)
+    DataFrame(np.hstack((curve_act,curve_normal_act))).to_csv('Curve_dense.csv',header=False,index=False)
 
-# diff=np.linalg.norm(np.diff(curve_act,axis=0),axis=1)
-# plt.figure()
-# plt.plot(diff)
-# plt.show()
+    # diff=np.linalg.norm(np.diff(curve_act,axis=0),axis=1)
+    # plt.figure()
+    # plt.plot(diff)
+    # plt.show()
 
-fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-ax.plot3D(curve_act[:,0],curve_act[:,1],curve_act[:,2],'r.-')
-ax.quiver(curve_act[:,0],curve_act[:,1],curve_act[:,2],curve_normal_act[:,0],curve_normal_act[:,1],curve_normal_act[:,2],length=1, normalize=True)
-plt.show()
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    ax.plot3D(curve_act[:,0],curve_act[:,1],curve_act[:,2],'r.-')
+    ax.quiver(curve_act[:,0],curve_act[:,1],curve_act[:,2],curve_normal_act[:,0],curve_normal_act[:,1],curve_normal_act[:,2],length=1, normalize=True)
+    plt.show()
 
-visualize_curve_w_normal(curve_act,curve_normal_act,stepsize=10)
+    visualize_curve_w_normal(curve_act,curve_normal_act,stepsize=10)
+
+
+if __name__ == '__main__':
+    main()
