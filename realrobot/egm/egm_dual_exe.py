cee60d572bbddee5343fd5fb53bdef10eada4d85
hehonglu123@126.com <hehonglu123@126.com>
2022-07-21 15:38:19 -0400

    egm debug

--- realrobot/egm/egm_dual_exe.py
+++ realrobot/egm/egm_dual_exe.py
@@ -1,45 +1,17 @@
 import numpy as np
 from general_robotics_toolbox import *
-import sys, threading
-sys.path.append('../../../toolbox')
+import sys
+sys.path.append('../../toolbox')
 from robots_def import *
 from error_check import *
 from MotionSend import *
 from lambda_calc import *
-sys.path.append('../../../toolbox/egm_toolbox')
+sys.path.append('../../toolbox/egm_toolbox')
 from EGM_toolbox import *
 import rpi_abb_irc5
 
-_lock=threading.Lock()
-_streaming=False
-curve_js2_exe=[]
-timestamp2=[]
-egm2=None
-def threadfunc():
-	global _streaming, curve_js2_exe, timestamp2, egm2
-	while(_streaming):
-		with _lock:
-			try:
-				res_i2, state_i2 = egm2.receive_from_robot()
-				if res_i2:
-					curve_js2_exe.append(np.radians(state_i2.joint_angles))
-					timestamp2.append(state_i2.robot_message.header.tm)
-			except:
-				traceback.print_exc()
-
-def StartStreaming():
-	global _streaming
-	_streaming=True
-	t=threading.Thread(target=threadfunc)
-	t.start()
-def StopStreaming():
-	global _streaming
-	_streaming=False
-
 def main():
-	global _streaming, curve_js2_exe, timestamp2, egm2
-
-	data_dir='../../../data/wood/'
+	data_dir='../../data/wood/'
 
 	robot1=abb6640(d=50)
 	with open(data_dir+'dual_arm/abb1200.yaml') as file:
@@ -64,65 +36,73 @@ def main():
 
 	vd=50
 	idx=et1.downsample24ms(relative_path,vd)
+	extension_start=40
+	extension_end=10
 
 	curve_cmd_js1=curve_js1[idx]
 	curve_cmd_js2=curve_js2[idx]
-	curve_js1_d=curve_js1[idx]
-	curve_js2_d=curve_js2[idx]
+	
+	##add extension
+	curve_cmd_js1_ext=et1.add_extension_egm_js(curve_cmd_js1,extension_start=extension_start,extension_end=extension_end)
+	curve_cmd_js2_ext=et1.add_extension_egm_js(curve_cmd_js2,extension_start=extension_start,extension_end=extension_end)
+
+	
+	iteration=100
+	adjust_weigt_it=20
 
 	###jog both arm to start pose
-	et1.jog_joint(curve_js1[0])
-	et2.jog_joint(curve_js2[0])
+	et1.jog_joint(curve_cmd_js1_ext[0])
+	et2.jog_joint(curve_cmd_js2_ext[0])
+
+
 
 	################################traverse curve for both arms#####################################
 
 	curve_exe_js1=[]
 	timestamp1=[]
 	curve_exe_js2=[]
+	timestamp2=[]
 	print('traversing trajectory')
 	try:
-		StartStreaming()
-		for i in range(len(curve_cmd_js1)):
+		for i in range(len(curve_cmd_js1_ext)):
 			while True:
 				res_i1, state_i1 = egm1.receive_from_robot()
 				if res_i1:
-					###send synchronously
-					send_res1 = egm1.send_to_robot(curve_cmd_js1[i])
-					#save joint angles
-					curve_exe_js1.append(np.radians(state_i1.joint_angles))
-					timestamp1.append(state_i1.robot_message.header.tm)
+					while True:
+						res_i2, state_i2 = egm2.receive_from_robot()
+						if res_i2:
+							send_res1 = egm1.send_to_robot(curve_cmd_js1_ext[i])
+							#save joint angles
+							curve_exe_js1.append(np.radians(state_i1.joint_angles))
+							timestamp1.append(state_i1.robot_message.header.tm)
 
-					send_res2 = egm2.send_to_robot(curve_cmd_js2[i])
+							
+
+							# res_i2, state_i2 = egm2.receive_from_robot()
+							send_res2 = egm2.send_to_robot(curve_cmd_js2_ext[i])
+							#save joint angles
+							curve_exe_js2.append(np.radians(state_i2.joint_angles))
+							timestamp2.append(state_i2.robot_message.header.tm)
+
+							break
 					break
 	except KeyboardInterrupt:
-		StopStreaming()
 		raise
-	StopStreaming()
-	time.sleep(1)
 
 	timestamp1=np.array(timestamp1)/1000
 	timestamp2=np.array(timestamp2)/1000
 
-	print(len(timestamp1))
-	print(len(timestamp2))
-	print(len(curve_cmd_js1))
-
-	print('robot1 duration: ', timestamp1[-1]-timestamp1[0])
-	print('robot2 duration: ', timestamp2[-1]-timestamp2[0])
-
-	plt.plot(timestamp1)
-	plt.show()
-
-	curve_exe_js1=np.array(curve_exe_js1)
-	curve_exe_js2=np.array(copy.deepcopy(curve_js2_exe))
-
 	##############################calcualte error########################################
-	relative_path_exe,relative_path_exe_R=form_relative_path(robot1,robot2,curve_exe_js1,curve_exe_js2,base2_R,base2_p)
+	relative_path_exe,relative_path_exe_R=form_relative_path(robot1,robot2,curve_exe_js1[extension_num+et1.idx_delay:-extension_num+et1.idx_delay],curve_exe_js2[extension_num+et1.idx_delay:-extension_num+et1.idx_delay],base2_R,base2_p)
 	lam=calc_lam_cs(relative_path_exe)
-	speed=np.gradient(lam)/np.gradient(timestamp1)
+	speed=np.gradient(lam)/np.gradient(timestamp1[extension_num+et1.idx_delay:-extension_num+et1.idx_delay])
 	error,angle_error=calc_all_error_w_normal(relative_path_exe,relative_path[:,:3],relative_path_exe_R[:,:,-1],relative_path[:,3:])
 	print(max(error))
 
+	plt.plot(timestamp1)
+	plt.show()
+	plt.plot(timestamp2)
+	plt.show()
 	##############################plot error#####################################
 
 	fig, ax1 = plt.subplots()
