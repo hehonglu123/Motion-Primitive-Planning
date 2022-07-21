b244713de37e98e23f3fa7711fad390dea73325f
Chen-Lung Eric Lu <eric565648@gmail.com>
2022-07-21 11:44:55 -0400

    dual arm

--- simulation/roboguide/fanuc_toolbox/fanuc_utils.py
+++ simulation/roboguide/fanuc_toolbox/fanuc_utils.py
@@ -6,14 +6,26 @@ from general_robotics_toolbox import *
 sys.path.append('../../../toolbox/')
 from utils import *
 from lambda_calc import *
+from robots_def import *
 
 class MotionSendFANUC(object):
-    def __init__(self,group=1,uframe=1,utool=2,robot_ip='127.0.0.2') -> None:
+    def __init__(self,group=1,uframe=1,utool=2,robot_ip='127.0.0.2',robot1=m710ic(d=50),robot2=m900ia(),group2=2,uframe2=1,utool2=2) -> None:
         
         self.client = FANUCClient(robot_ip)
+        
+        # robot1 roboguide info
         self.group = group
         self.uframe = uframe
         self.utool = utool
+
+        # robot2 roboguide info
+        self.group2 = group2
+        self.uframe2 = uframe2
+        self.utool2 = utool2
+
+        # robots
+        self.robot1=robot1
+        self.robot2=robot2
     
     def exec_motions(self,robot,primitives,breakpoints,p_bp,q_bp,speed,zone):
 
@@ -44,6 +56,51 @@ class MotionSendFANUC(object):
                 tp.moveJ(robt,speed,'%',this_zone)
         
         return self.client.execute_motion_program(tp)
+
+    def exec_motions_multimove(self,robot1,robot2,primitives1,primitives2,p_bp1,p_bp2,q_bp1,q_bp2,speed,zone):
+
+        tp_follow = TPMotionProgram()
+        tp_lead = TPMotionProgram()
+        #### move to start
+        # robot1
+        j0 = joint2robtarget(q_bp1[0][0],robot1,self.group,self.uframe,self.utool)
+        tp_follow.moveJ(j0,50,'%',-1)
+        tp_follow.moveJ(j0,5,'%',-1)
+        # robot2
+        j0 = joint2robtarget(q_bp2[0][0],robot2,self.group2,self.uframe2,self.utool2)
+        tp_lead.moveJ(j0,50,'%',-1)
+        tp_lead.moveJ(j0,5,'%',-1)
+        self.client.execute_motion_program_coord(tp_lead,tp_follow)
+
+        #### start traj
+        tp_follow = TPMotionProgram()
+        tp_lead = TPMotionProgram()
+        for i in range(1,len(primitives1)):
+            if i == len(primitives1)-1:
+                this_zone = -1
+            else:
+                this_zone = zone
+            if primitives1[i]=='movel_fit':
+                # robot1
+                robt1 = joint2robtarget(q_bp1[i][0],robot1,self.group,self.uframe,self.utool)
+                tp_follow.moveL(robt1,speed,'mmsec',this_zone,'COORD')
+                # robot2
+                robt2 = joint2robtarget(q_bp2[i][0],robot2,self.group2,self.uframe2,self.utool2)
+                tp_lead.moveL(robt2,speed,'mmsec',this_zone,'COORD')
+            elif primitives1[i]=='movec_fit':
+                # robot1
+                robt_mid1 = joint2robtarget(q_bp1[i][0],robot1,self.group,self.uframe,self.utool)
+                robt1 = joint2robtarget(q_bp1[i][1],robot1,self.group,self.uframe,self.utool)
+                tp_follow.moveC(robt_mid1,robt1,speed,'mmsec',this_zone,'COORD')
+                # robot2
+                robt_mid2 = joint2robtarget(q_bp2[i][0],robot2,self.group2,self.uframe2,self.utool2)
+                robt2 = joint2robtarget(q_bp2[i][1],robot2,self.group2,self.uframe2,self.utool2)
+                tp_lead.moveC(robt_mid2,robt2,speed,'mmsec',this_zone,'COORD')
+            # else: #moveJ
+            #     robt = jointtarget(self.group,self.uframe,self.utool,np.degrees(q_bp[i][0]),[0]*6)
+            #     tp.moveJ(robt,speed,'%',this_zone)
+        
+        return self.client.execute_motion_program_coord(tp_lead,tp_follow)
     
     def logged_data_analysis(self,robot,df):
 
@@ -98,8 +155,83 @@ class MotionSendFANUC(object):
         curve_exe_js_act=np.array(curve_exe_js_act)
         act_speed = np.array(act_speed)
 
-        return lam_exec, curve_exe, curve_exe_R,curve_exe_js_act, act_speed, timestamp
+        return lam_exec, curve_exe, curve_exe_R,curve_exe_js_act, act_speed, timestamp_act
     
+    def logged_data_analysis_multimove(self,df,base2_R,base2_p,realrobot=False):
+        q1_1=df['J11'].tolist()[1:-1]
+        q1_2=df['J12'].tolist()[1:-1]
+        q1_3=df['J13'].tolist()[1:-1]
+        q1_4=df['J14'].tolist()[1:-1]
+        q1_5=df['J15'].tolist()[1:-1]
+        q1_6=df['J16'].tolist()[1:-1]
+        q2_1=df['J21'].tolist()[1:-1]
+        q2_2=df['J22'].tolist()[1:-1]
+        q2_3=df['J23'].tolist()[1:-1]
+        q2_4=df['J24'].tolist()[1:-1]
+        q2_5=df['J25'].tolist()[1:-1]
+        q2_6=df['J26'].tolist()[1:-1]
+        timestamp=np.array(df['timestamp'].tolist()[1:]).astype(float)*1e-3 # from msec to sec
+
+        curve_exe_js1=np.radians(np.vstack((q1_1,q1_2,q1_3,q1_4,q1_5,q1_6)).T.astype(float))
+        curve_exe_js2=np.radians(np.vstack((q2_1,q2_2,q2_3,q2_4,q2_5,q2_6)).T.astype(float))
+
+        if realrobot:
+            timestamp, curve_exe_js_all=lfilter(timestamp, np.hstack((curve_exe_js1,curve_exe_js2)))
+            curve_exe_js1=curve_exe_js_all[:,:6]
+            curve_exe_js2=curve_exe_js_all[:,6:]
+
+        act_speed=[0]
+        lam=[0]
+        relative_path_exe=[]
+        relative_path_exe_R=[]
+        curve_exe1=[]
+        curve_exe2=[]
+        curve_exe_R1=[]
+        curve_exe_R2=[]
+        curve_exe_js1_act=[]
+        curve_exe_js2_act=[]
+        timestamp_act = []
+        last_cont = False
+        for i in range(len(curve_exe_js1)):
+            if i>5 and i<len(curve_exe_js1)-5:
+                # if the recording is not fast enough
+                # then having to same logged joint angle
+                # do interpolation for estimation
+                if np.all(curve_exe_js1[i]==curve_exe_js1[i+1]) and np.all(curve_exe_js2[i]==curve_exe_js2[i+1]):
+                    last_cont = True
+                    continue
+
+            curve_exe_js1_act.append(curve_exe_js1[i])
+            curve_exe_js2_act.append(curve_exe_js2[i])
+            timestamp_act.append(timestamp[i])
+            pose1_now=self.robot1.fwd(curve_exe_js1[i])
+            pose2_now=self.robot2.fwd(curve_exe_js2[i])
+            # curve in robot's own frame
+            curve_exe1.append(pose1_now.p)
+            curve_exe2.append(pose2_now.p)
+            curve_exe_R1.append(pose1_now.R)
+            curve_exe_R2.append(pose2_now.R)
+
+            pose2_world_now=self.robot2.fwd(curve_exe_js2[i],base2_R,base2_p)
+            relative_path_exe.append(np.dot(pose2_world_now.R.T,pose1_now.p-pose2_world_now.p))
+            relative_path_exe_R.append(pose2_world_now.R.T@pose1_now.R)
+            
+            if i>0:
+                lam.append(lam[-1]+np.linalg.norm(relative_path_exe[-1]-relative_path_exe[-2]))
+            try:
+                if timestamp[-1]!=timestamp[-2]:
+                    if last_cont:
+                        timestep=timestamp[i]-timestamp[i-2]
+                    else:
+                        timestep=timestamp[i]-timestamp[i-1]
+                    act_speed.append(np.linalg.norm(relative_path_exe[-1]-relative_path_exe[-2])/timestep)
+                    
+            except IndexError:
+                pass
+            last_cont = False
+
+        return np.array(lam), np.array(curve_exe1),np.array(curve_exe2), np.array(curve_exe_R1),np.array(curve_exe_R2),curve_exe_js1_act,curve_exe_js2_act, act_speed, timestamp_act, np.array(relative_path_exe), np.array(relative_path_exe_R)
+
     def chop_extension(self,curve_exe, curve_exe_R,curve_exe_js, speed, timestamp,curve,curve_normal):
         start_idx=np.argmin(np.linalg.norm(curve[0,:]-curve_exe,axis=1))+1
         end_idx=np.argmin(np.linalg.norm(curve[-1,:]-curve_exe,axis=1))
@@ -117,14 +249,68 @@ class MotionSendFANUC(object):
         timestamp=timestamp[start_idx:end_idx+1]
         lam=calc_lam_cs(curve_exe)
         return lam, curve_exe, curve_exe_R,curve_exe_js, speed, timestamp
+    
+    def chop_extension_dual(self,lam, curve_exe1,curve_exe2,curve_exe_R1,curve_exe_R2,curve_exe_js1,curve_exe_js2, speed, timestamp, relative_path_exe,relative_path_exe_R,p_start,p_end):
+        start_idx=np.argmin(np.linalg.norm(p_start-relative_path_exe,axis=1))+1
+        end_idx=np.argmin(np.linalg.norm(p_end-relative_path_exe,axis=1))
 
-def extend_start_end(robot,q_bp,primitives,breakpoints,points_list,extension_d=100):
+        #make sure extension doesn't introduce error
+        if np.linalg.norm(relative_path_exe[start_idx]-p_start)>0.5:
+            start_idx+=1
+        if np.linalg.norm(relative_path_exe[end_idx]-p_end)>0.5:
+            end_idx-=1
+
+        curve_exe1=curve_exe1[start_idx:end_idx+1]
+        curve_exe2=curve_exe2[start_idx:end_idx+1]
+        curve_exe_R1=curve_exe_R1[start_idx:end_idx+1]
+        curve_exe_R2=curve_exe_R2[start_idx:end_idx+1]
+        curve_exe_js1=curve_exe_js1[start_idx:end_idx+1]
+        curve_exe_js2=curve_exe_js2[start_idx:end_idx+1]
+
+        relative_path_exe=relative_path_exe[start_idx:end_idx+1]
+        relative_path_exe_R=relative_path_exe_R[start_idx:end_idx+1]
+
+        speed=speed[start_idx:end_idx+1]
+        lam=calc_lam_cs(relative_path_exe)
+
+        return lam, curve_exe1,curve_exe2,curve_exe_R1,curve_exe_R2,curve_exe_js1,curve_exe_js2, speed, timestamp, relative_path_exe,relative_path_exe_R
+    
+    def form_relative_path(self,curve_js1,curve_js2,base2_R,base2_p):
+        relative_path_exe=[]
+        relative_path_exe_R=[]
+        curve_exe1=[]
+        curve_exe2=[]
+        curve_exe_R1=[]
+        curve_exe_R2=[]
+        for i in range(len(curve_js1)):
+            pose1_now=self.robot1.fwd(curve_js1[i])
+            pose2_now=self.robot2.fwd(curve_js2[i])
+
+            curve_exe1.append(pose1_now.p)
+            curve_exe2.append(pose2_now.p)
+            curve_exe_R1.append(pose1_now.R)
+            curve_exe_R2.append(pose2_now.R)
+
+            pose2_world_now=self.robot2.fwd(curve_js2[i],base2_R,base2_p)
+
+
+            relative_path_exe.append(np.dot(pose2_world_now.R.T,pose1_now.p-pose2_world_now.p))
+            relative_path_exe_R.append(pose2_world_now.R.T@pose1_now.R)
+        return np.array(relative_path_exe),np.array(relative_path_exe_R)
+
+    def calc_robot2_q_from_blade_pose(self,blade_pose,base2_R,base2_p):
+        R2=base2_R.T@blade_pose[:3,:3]
+        p2=-base2_R.T@(base2_p-blade_pose[:3,-1])
+
+        return self.robot2.inv(p2,R2)[1]
+
+def extend_start_end(robot,q_bp,primitives,breakpoints,points_list,extension_start=100,extension_end=100):
     
     dist_bp = np.linalg.norm(np.array(points_list[0][0])-np.array(points_list[1][0]))
-    if dist_bp > extension_d:
-        dist_bp=extension_d
-    step_to_extend=round(extension_d/dist_bp)
-    extend_step_d=float(extension_d)/step_to_extend
+    if dist_bp > extension_start:
+        dist_bp=extension_start
+    step_to_extend=round(extension_start/dist_bp)
+    extend_step_d_start=float(extension_start)/step_to_extend
     
     ###initial point extension
     pose_start=robot.fwd(q_bp[0][-1])
@@ -137,16 +323,16 @@ def extend_start_end(robot,q_bp,primitives,breakpoints,points_list,extension_d=1
         #find new start point
         slope_p=p_end-p_start
         slope_p=slope_p/np.linalg.norm(slope_p)
-        p_start_new=p_start-extension_d*slope_p        ###extend 5cm backward
+        p_start_new=p_start-extension_start*slope_p        ###extend 5cm backward
 
         #find new start orientation
         k,theta=R2rot(R_end@R_start.T)
-        theta_new=-extension_d*theta/np.linalg.norm(p_end-p_start)
+        theta_new=-extension_start*theta/np.linalg.norm(p_end-p_start)
         # R_start_new=rot(k,theta_new)@R_start
 
         # adding extension with uniform space
         for i in range(1,step_to_extend+1):
-            p_extend=p_start-i*extend_step_d*slope_p
+            p_extend=p_start-i*extend_step_d_start*slope_p
             theta_extend=-np.linalg.norm(p_extend-p_start)*theta/np.linalg.norm(p_end-p_start)
             R_extend=rot(k,theta_extend)@R_start
             points_list.insert(0,[p_extend])
@@ -165,7 +351,7 @@ def extend_start_end(robot,q_bp,primitives,breakpoints,points_list,extension_d=1
         center, radius=circle_from_3point(p_start,p_end,p_mid)
 
         #find desired rotation angle
-        angle=extension_d/radius
+        angle=extension_start/radius
 
         #find new start point
         plane_N=np.cross(p_end-center,p_start-center)
@@ -175,7 +361,7 @@ def extend_start_end(robot,q_bp,primitives,breakpoints,points_list,extension_d=1
 
         #find new start orientation
         k,theta=R2rot(R_end@R_start.T)
-        theta_new=-extension_d*theta/np.linalg.norm(p_end-p_start)
+        theta_new=-extension_start*theta/np.linalg.norm(p_end-p_start)
         R_start_new=rot(k,theta_new)@R_start
 
         #solve invkin for initial point
@@ -187,7 +373,7 @@ def extend_start_end(robot,q_bp,primitives,breakpoints,points_list,extension_d=1
         J_start=robot.jacobian(q_bp[0][0])
         qdot=q_bp[1][0]-q_bp[0][0]
         v=J_start[3:,:]@qdot
-        t=extension_d/np.linalg.norm(v)
+        t=extension_start/np.linalg.norm(v)
         
         q_bp[0][0]=q_bp[0][0]+qdot*t
         points_list[0][0]=robot.fwd(q_bp[0][0]).p
@@ -199,6 +385,7 @@ def extend_start_end(robot,q_bp,primitives,breakpoints,points_list,extension_d=1
     pose_end=robot.fwd(q_bp[-1][-1])
     p_end=pose_end.p
     R_end=pose_end.R
+    extend_step_d_end=float(extension_end)/step_to_extend
 
     if primitives[-1]=='movel_fit':
         #find new end point
@@ -213,7 +400,7 @@ def extend_start_end(robot,q_bp,primitives,breakpoints,points_list,extension_d=1
 
         # adding extension with uniform space
         for i in range(1,step_to_extend+1):
-            p_extend=p_end+i*extend_step_d*slope_p
+            p_extend=p_end+i*extend_step_d_end*slope_p
             theta_extend=np.linalg.norm(p_extend-p_end)*theta/np.linalg.norm(p_end-p_start)
             R_extend=rot(k,theta_extend)@R_end
             points_list.append([p_extend])
@@ -233,7 +420,7 @@ def extend_start_end(robot,q_bp,primitives,breakpoints,points_list,extension_d=1
         center, radius=circle_from_3point(p_start,p_end,p_mid)
 
         #find desired rotation angle
-        angle=extension_d/radius
+        angle=extension_end/radius
 
         #find new end point
         plane_N=np.cross(p_start-center,p_end-center)
@@ -243,7 +430,7 @@ def extend_start_end(robot,q_bp,primitives,breakpoints,points_list,extension_d=1
 
         #find new end orientation
         k,theta=R2rot(R_end@R_start.T)
-        theta_new=extension_d*theta/np.linalg.norm(p_end-p_start)
+        theta_new=extension_end*theta/np.linalg.norm(p_end-p_start)
         R_end_new=rot(k,theta_new)@R_end
 
         #solve invkin for end point
@@ -255,12 +442,24 @@ def extend_start_end(robot,q_bp,primitives,breakpoints,points_list,extension_d=1
         J_end=robot.jacobian(q_bp[-1][0])
         qdot=q_bp[-1][0]-q_bp[-2][0]
         v=J_end[3:,:]@qdot
-        t=extension_d/np.linalg.norm(v)
+        t=extension_end/np.linalg.norm(v)
         
         q_bp[-1][-1]=q_bp[-1][-1]+qdot*t
         points_list[-1][0]=robot.fwd(q_bp[-1][-1]).p
 
     return primitives,points_list,q_bp
+
+def extend_dual(robot1,p_bp1,q_bp1,primitives1,robot2,p_bp2,q_bp2,primitives2,breakpoints,extension_d=100):
+    #extend porpotionally
+    d1_start=np.linalg.norm(p_bp1[1][-1]-p_bp1[0][-1])
+    d2_start=np.linalg.norm(p_bp2[1][-1]-p_bp2[0][-1])
+    d1_end=np.linalg.norm(p_bp1[-1][-1]-p_bp1[-2][-1])
+    d2_end=np.linalg.norm(p_bp2[-1][-1]-p_bp2[-2][-1])
+
+    primitives1,p_bp1,q_bp1=extend_start_end(robot1,q_bp1,primitives1,breakpoints,p_bp1,extension_start=extension_d*d1_start/d2_start,extension_end=extension_d*d1_end/d2_end)
+    primitives2,p_bp2,q_bp2=extend_start_end(robot2,q_bp2,primitives2,breakpoints,p_bp2,extension_start=extension_d,extension_end=extension_d)
+
+    return p_bp1,q_bp1,p_bp2,q_bp2
 
 def extract_data_from_cmd(filename):
     data = read_csv(filename)
@@ -306,7 +505,7 @@ def extract_points(primitive_type,points):
     else:
         if points[1] == '[':
             endpoint=points[2:-2].split(',')
-            return list(map(float, endpoint)) 
+            return np.array(list(map(float, endpoint)))
         else:
             endpoint=points[8:-3].split(',')
-            return list(map(float, endpoint))   
+            return np.array(list(map(float, endpoint)))
---
+++ simulation/roboguide/ilc_fanuc/max_gradient_dual.py
@@ -0,0 +1,197 @@
+########
+# This module utilized https://github.com/johnwason/abb_motion_program_exec
+# and send whatever the motion primitives that algorithms generate
+# to RobotStudio
+########
+
+import numpy as np
+from general_robotics_toolbox import *
+from pandas import read_csv
+import sys
+from io import StringIO
+from scipy.signal import find_peaks
+import yaml
+from matplotlib import pyplot as plt
+from pathlib import Path
+from fanuc_motion_program_exec_client import *
+sys.path.append('../fanuc_toolbox')
+from fanuc_utils import *
+sys.path.append('../../../ILC')
+from ilc_toolbox import *
+sys.path.append('../../../toolbox')
+from robots_def import *
+from error_check import *
+from lambda_calc import *
+from blending import *
+
+def main():
+    # curve
+    # data_type='blade'
+    data_type='wood'
+
+    # data and curve directory
+    if data_type=='blade':
+        curve_data_dir='../../../data/from_NX/'
+        cmd_dir='../data/curve_blade/'
+        data_dir='data/blade_dual/'
+    elif data_type=='wood':
+        curve_data_dir='../../../data/wood/'
+        cmd_dir='../data/curve_wood/'
+        data_dir='data/wood_dual/'
+
+    # relative path
+    relative_path = read_csv(curve_data_dir+"/Curve_dense.csv", header=None).values
+
+    # the second robot relative to the fist robot
+    with open(cmd_dir+'dual_arm/m900ia.yaml') as file:
+        H_robot2 = np.array(yaml.safe_load(file)['H'],dtype=np.float64)
+    base2_R=H_robot2[:3,:3]
+    base2_p=1000*H_robot2[:-1,-1]
+    # workpiece (curve) relative to robot tcp
+    with open(cmd_dir+'dual_arm/tcp.yaml') as file:
+        H_tcp = np.array(yaml.safe_load(file)['H'],dtype=np.float64)
+    
+    # define robot
+    robot1=m710ic(d=50)
+    robot2=m900ia(R_tool=H_tcp[:3,:3],p_tool=H_tcp[:-1,-1])
+
+    # fanuc motion send tool
+    if data_type=='blade':
+        ms = MotionSendFANUC(robot1=robot1,robot2=robot2)
+    elif data_type=='wood':
+        ms = MotionSendFANUC(robot1=robot1,robot2=robot2,utool2=3)
+
+    s=1000 # mm/sec in leader frame
+    z=100 # CNT100
+    ilc_output=data_dir+'results_'+str(s)+'/'
+    Path(ilc_output).mkdir(exist_ok=True)
+
+    breakpoints1,primitives1,p_bp1,q_bp1=extract_data_from_cmd(os.getcwd()+'/'+cmd_dir+'/dual_arm/command1.csv')
+    breakpoints2,primitives2,p_bp2,q_bp2=extract_data_from_cmd(os.getcwd()+'/'+cmd_dir+'/dual_arm/command2.csv')
+
+    ###extension
+    p_bp1,q_bp1,p_bp2,q_bp2=extend_dual(ms.robot1,p_bp1,q_bp1,primitives1,ms.robot2,p_bp2,q_bp2,primitives2,breakpoints1,extension_d=100)
+
+    ###ilc toolbox def
+    ilc=ilc_toolbox([robot1,robot2],[primitives1,primitives2],base2_R,base2_p)
+
+    multi_peak_threshold=0.2
+    ###TODO: extension fix start point, moveC support
+    iteration=10
+    draw_y_max=None
+    max_error_tolerance = 0.5
+    for i in range(iteration):
+
+        ###execution with plant
+        logged_data=ms.exec_motions_multimove(robot1,robot2,primitives1,primitives2,p_bp1,p_bp2,q_bp1,q_bp2,s,z)
+        # with open('iteration_'+str(i)+'.csv',"wb") as f:
+        #     f.write(logged_data)
+        StringData=StringIO(logged_data.decode('utf-8'))
+        df = read_csv(StringData, sep =",")
+        ##############################data analysis#####################################
+        lam, curve_exe1,curve_exe2,curve_exe_R1,curve_exe_R2,curve_exe_js1,curve_exe_js2, speed, timestamp, relative_path_exe, relative_path_exe_R = ms.logged_data_analysis_multimove(df,base2_R,base2_p,realrobot=False)
+        #############################chop extension off##################################
+        lam, curve_exe1,curve_exe2,curve_exe_R1,curve_exe_R2,curve_exe_js1,curve_exe_js2, speed, timestamp, relative_path_exe, relative_path_exe_R=\
+            ms.chop_extension_dual(lam, curve_exe1,curve_exe2,curve_exe_R1,curve_exe_R2,curve_exe_js1,curve_exe_js2, speed, timestamp, relative_path_exe,relative_path_exe_R,relative_path[0,:3],relative_path[-1,:3])
+        ave_speed=np.mean(speed)
+
+        ##############################calcualte error########################################
+        error,angle_error=calc_all_error_w_normal(relative_path_exe,relative_path[:,:3],relative_path_exe_R[:,:,-1],relative_path[:,3:])
+        print('Iteration:',i,', Max Error:',max(error),'Ave. Speed:',ave_speed,'Std. Speed:',np.std(speed),'Std/Ave (%):',np.std(speed)/ave_speed*100)
+        print('Max Speed:',max(speed),'Min Speed:',np.min(speed),'Ave. Error:',np.mean(error),'Min Error:',np.min(error),"Std. Error:",np.std(error))
+        print('Max Ang Error:',max(np.degrees(angle_error)),'Min Ang Error:',np.min(np.degrees(angle_error)),'Ave. Ang Error:',np.mean(np.degrees(angle_error)),"Std. Ang Error:",np.std(np.degrees(angle_error)))
+        print("===========================================")
+        #############################error peak detection###############################
+        find_peak_dist = 20/(lam[int(len(lam)/2)]-lam[int(len(lam)/2)-1])
+        if find_peak_dist<1:
+            find_peak_dist=1
+        peaks,_=find_peaks(error,height=multi_peak_threshold,prominence=0.05,distance=find_peak_dist)		###only push down peaks higher than height, distance between each peak is 20mm, threshold to filter noisy peaks
+        if len(peaks)==0 or np.argmax(error) not in peaks:
+            peaks=np.append(peaks,np.argmax(error))
+
+        # peaks=np.array([np.argmax(error)])
+        ##############################plot error#####################################
+        fig, ax1 = plt.subplots()
+        ax2 = ax1.twinx()
+        ax1.plot(lam, speed, 'g-', label='Speed')
+        ax2.plot(lam, error, 'b-',label='Error')
+        ax2.scatter(lam[peaks],error[peaks],label='peaks')
+        ax2.plot(lam, np.degrees(angle_error), 'y-',label='Normal Error')
+        if draw_y_max is None:
+            draw_y_max=max(error)*1.05
+        ax2.axis(ymin=0,ymax=draw_y_max)
+        ax1.set_xlabel('lambda (mm)')
+        ax1.set_ylabel('Speed/lamdot (mm/s)', color='g')
+        ax2.set_ylabel('Error/Normal Error (mm/deg)', color='b')
+        plt.title("Speed and Error Plot")
+        ax1.legend(loc=0)
+        ax2.legend(loc=0)
+        plt.legend()
+        # plt.savefig(ilc_output+'iteration_ '+str(i))
+        # plt.clf()
+        plt.show()
+
+        # plt.figure()
+        # ax = plt.axes(projection='3d')
+        # ax.plot3D(relative_path[:,0], relative_path[:,1],relative_path[:,2], 'red',label='original')
+        # ax.scatter3D(relative_path[breakpoints1[:-1],0], relative_path[breakpoints1[:-1],1],relative_path[breakpoints1[:-1],2], 'blue')
+        # ax.plot3D(relative_path_exe[:,0], relative_path_exe[:,1],relative_path_exe[:,2], 'green',label='execution')
+        # plt.legend()
+        # plt.show()
+        exit()
+        ###########################plot for verification###################################
+        # p_bp_relative,_=ms.form_relative_path(np.squeeze(q_bp1),np.squeeze(q_bp2),base2_R,base2_p)
+        # plt.figure()
+        # ax = plt.axes(projection='3d')
+        # ax.plot3D(relative_path[:,0], relative_path[:,1], relative_path[:,2], c='gray',label='original')
+        # ax.plot3D(relative_path_exe[:,0], relative_path_exe[:,1], relative_path_exe[:,2], c='red',label='execution')
+        # ax.scatter3D(p_bp_relative[:,0], p_bp_relative[:,1], p_bp_relative[:,2], c=p_bp_relative[:,2], cmap='Greens',label='breakpoints')
+        # ax.scatter(relative_path_exe[peaks,0], relative_path_exe[peaks,1], relative_path_exe[peaks,2],c='orange',label='worst case')
+        # plt.show()
+
+        ##########################################calculate gradient for peaks######################################
+        ###restore trajectory from primitives
+        curve_interp1, curve_R_interp1, curve_js_interp1, breakpoints_blended=form_traj_from_bp(q_bp1,primitives1,robot1)
+        curve_interp2, curve_R_interp2, curve_js_interp2, breakpoints_blended=form_traj_from_bp(q_bp2,primitives2,robot2)
+        curve_js_blended1,curve_blended1,curve_R_blended1=blend_js_from_primitive(curve_interp1, curve_js_interp1, breakpoints_blended, primitives1,robot1,zone=10)
+        curve_js_blended2,curve_blended2,curve_R_blended2=blend_js_from_primitive(curve_interp2, curve_js_interp2, breakpoints_blended, primitives2,robot2,zone=10)
+
+        ###establish relative trajectory from blended trajectory
+        relative_path_blended,relative_path_blended_R=ms.form_relative_path(curve_js_blended1,curve_js_blended2,base2_R,base2_p)
+
+        for peak in peaks:
+            ######gradient calculation related to nearest 3 points from primitive blended trajectory, not actual one
+            _,peak_error_curve_idx=calc_error(relative_path_exe[peak],relative_path[:,:3])  # index of original curve closest to max error point
+
+            ###get closest to worst case point on blended trajectory
+            _,peak_error_curve_blended_idx=calc_error(relative_path_exe[peak],relative_path_blended)
+
+            ###############get numerical gradient#####
+            ###find closest 3 breakpoints
+            order=np.argsort(np.abs(breakpoints_blended-peak_error_curve_blended_idx))
+            breakpoint_interp_2tweak_indices=order[:3]
+
+            de_dp=ilc.get_gradient_from_model_xyz_dual(\
+                [p_bp1,p_bp2],[q_bp1,q_bp2],breakpoints_blended,[curve_blended1,curve_blended2],peak_error_curve_blended_idx,[curve_exe_js1[peak],curve_exe_js2[peak]],relative_path[peak_error_curve_idx,:3],breakpoint_interp_2tweak_indices)
+
+
+            p_bp1_new, q_bp1_new,p_bp2_new,q_bp2_new=ilc.update_bp_xyz_dual([p_bp1,p_bp2],[q_bp1,q_bp2],de_dp,error[peak],breakpoint_interp_2tweak_indices)
+
+            #########plot adjusted breakpoints
+            p_bp_relative_new,_=ms.form_relative_path(np.squeeze(q_bp1_new),np.squeeze(q_bp2_new),base2_R,base2_p)
+
+
+            # ax.scatter3D(p_bp_relative_new[breakpoint_interp_2tweak_indices,0], p_bp_relative_new[breakpoint_interp_2tweak_indices,1], p_bp_relative_new[breakpoint_interp_2tweak_indices,2], c='blue',label='new breakpoints')
+            # plt.legend()
+            # plt.show()
+
+            ###update
+            p_bp1=p_bp1_new
+            q_bp1=q_bp1_new
+            p_bp2=p_bp2_new
+            q_bp2=q_bp2_new
+
+
+
+if __name__ == "__main__":
+    main()
