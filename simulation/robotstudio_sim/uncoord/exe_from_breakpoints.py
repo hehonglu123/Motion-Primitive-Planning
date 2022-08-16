8787fdf5a99111ab9ea52c6e515b28c15b0eedee
hehonglu123 <hehonglu123@126.com>
2022-08-15 22:49:06 -0400

    uncoordinated

--- simulation/robotstudio_sim/uncoord/exe_from_breakpoints.py
+++ simulation/robotstudio_sim/uncoord/exe_from_breakpoints.py
@@ -1,59 +1,60 @@
 import abb_motion_program_exec_client as abb
+from threading import Thread
+
+from io import StringIO
+
 
 j1 = abb.jointtarget([10,20,30,40,50,60],[0]*6)
-j2 = abb.jointtarget([90,-91,60,-93,94,-95],[0]*6)
+j2 = abb.jointtarget([90,-60,60,-93,94,-95],[0]*6)
 j3 = abb.jointtarget([-80,81,-82,83,-84,85],[0]*6)
 
 my_tool = abb.tooldata(True,abb.pose([0,0,0.1],[1,0,0,0]),abb.loaddata(0.001,[0,0,0.001],[1,0,0,0],0,0,0)) 
 
 mp = abb.MotionProgram(tool=my_tool)
-mp.MoveAbsJ(j1,abb.v1000,abb.fine)
-mp.MoveAbsJ(j2,abb.v5000,abb.fine)
-mp.MoveAbsJ(j3,abb.v500,abb.fine)
-mp.MoveAbsJ(j2,abb.v5000,abb.z50)
-mp.MoveAbsJ(j3,abb.v500,abb.z200)
-mp.MoveAbsJ(j2,abb.v5000,abb.fine)
-mp.WaitTime(1)
-
-r1 = abb.robtarget([0.1649235*1e3, 0.1169957*1e3, 0.9502961*1e3], [ 0.6776466, -0.09003431, 0.6362069, 0.3576725 ], abb.confdata(0,0,0,0),[0]*6)
-r2 = abb.robtarget([ 0.6243948*1e3, -0.479558*1e3 ,  0.7073749*1e3], [ 0.6065634, -0.2193409,  0.6427138, -0.4133877], abb.confdata(-1,-1,0,1),[0]*6)
-
-r3 = abb.robtarget([417.9236, 276.9956, 885.2959], [ 0.8909725 , -0.1745558 ,  0.08864544,  0.4096832 ], abb.confdata( 0.,  1., -2.,  0.),[0]*6)
-r4 = abb.robtarget([417.9235 , -11.00438, 759.2958 ], [0.7161292 , 0.1868255 , 0.01720813, 0.6722789 ], abb.confdata( 0.,  2., -2.,  0.),[0]*6)
-r5 = abb.robtarget([ 417.9235, -173.0044,  876.2958], [0.6757616, 0.3854275, 0.2376617, 0.5816431], abb.confdata(-1.,  1., -1.,  0.),[0]*6)
-
-mp.MoveJ(r1,abb.v500,abb.fine)
-mp.MoveJ(r2,abb.v400,abb.fine)
-mp.MoveJ(r1,abb.v500,abb.z100)
-mp.MoveJ(r2,abb.v400,abb.z100)
-mp.MoveJ(r1,abb.v500,abb.fine)
-mp.WaitTime(1.5)
-
-mp.MoveJ(r3,abb.v5000,abb.fine)
-mp.MoveL(r4,abb.v200,abb.fine)
-mp.MoveL(r3,abb.v200,abb.fine)
-mp.MoveL(r4,abb.v1000,abb.z100)
-mp.MoveL(r3,abb.v1000,abb.z100)
-mp.MoveL(r4,abb.v1000,abb.fine)
-mp.WaitTime(2.5)
-
-mp.MoveJ(r3,abb.v5000,abb.fine)
-
-mp.MoveC(r4,r5,abb.v200,abb.z10)
-mp.MoveC(r4,r3,abb.v50,abb.fine)
-
-# Print out RAPID module of motion program for debugging
-print(mp.get_program_rapid())
+mp.MoveAbsJ(j1,abb.vmax,abb.fine)
+mp.MoveAbsJ(j2,abb.vmax,abb.fine)
+mp.MoveAbsJ(j3,abb.vmax,abb.fine)
 
 # Execute the motion program on the robot
 # Change base_url to the robot IP address
-client = abb.MotionProgramExecClient(base_url="http://192.168.68.68:80")
-log_results = client.execute_motion_program(mp)
+client1 = abb.MotionProgramExecClient(base_url="http://192.168.68.68:80")
+client2 = abb.MotionProgramExecClient()
 
-# Write log csv to file
-with open("log.csv","wb") as f:
-    f.write(log_results)
+log_results1=None
+log_results2=None
+def move_robot1():
+    global log_results1, df1, mp
+    log_results1 = client1.execute_motion_program(mp)
 
-# Or convert to string and use in memory
-log_results_str = log_results.decode('ascii')
-print(log_results_str)
+def move_robot2():
+    global log_results2, df2, mp
+    log_results2 = client2.execute_motion_program(mp)
+
+def main():
+    global log_results1, log_results2, client1, client2
+
+    ###move to start
+    mp = abb.MotionProgram(tool=my_tool)
+    mp.MoveAbsJ(j1,abb.v1000,abb.fine)
+    client1.execute_motion_program(mp)
+    client2.execute_motion_program(mp)
+
+    t1 = Thread(target=move_robot1)
+    t2 = Thread(target=move_robot2)
+
+    # start the threads
+    t1.start()
+    t2.start()
+
+    # wait for the threads to complete
+    t1.join()
+    t2.join()
+
+    with open("recorded_data/log1_5.csv","wb") as f:
+        f.write(log_results1)
+
+    with open("recorded_data/log2_5.csv","wb") as f:
+        f.write(log_results2)
+
+if __name__ == '__main__':
+    main()
