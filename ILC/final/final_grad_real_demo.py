6d54ff784dadd56cd797100fb96ec3c89192d55f
hehonglu123@126.com <hehonglu123@126.com>
2022-09-14 17:09:46 -0400

    final real recording

--- ILC/final/final_grad_real_demo.py
+++ ILC/final/final_grad_real_demo.py
@@ -62,7 +62,7 @@ def main():
 	plt.legend()
 	plt.title("Spatial Curve and Simulation Tuned Breakpoints")
 	plt.show(block=False)
-	fig_3d.canvas.manager.window.move(1300,566)
+	fig_3d.canvas.manager.window.move(1111,400)
 	plt.pause(0.1)
 
 	max_error_prev=999
@@ -111,7 +111,7 @@ def main():
 		h2, l2 = ax2.get_legend_handles_labels()
 		ax1.legend(h1+h2, l1+l2, loc=1)
 		
-		fig_error_speed.canvas.manager.window.move(1300,99)
+		fig_error_speed.canvas.manager.window.move(922,-77)
 		plt.show(block=False)
 		plt.pause(0.1)
 
@@ -129,7 +129,7 @@ def main():
 		ax.scatter(p_bp_np[:,0], p_bp_np[:,1], p_bp_np[:,2], c='green',label='breakpoints')
 		plt.legend()
 		plt.title("Iteration "+str(i+1)+": 3D Plot")
-		fig_3d.canvas.manager.window.move(1300,566)
+		fig_3d.canvas.manager.window.move(1111,400)
 		plt.show(block=False)
 		plt.pause(0.1)
 		
@@ -138,29 +138,32 @@ def main():
 			print('all bps adjustment')
 			##########################################adjust bp's toward error direction######################################
 			error_bps_v,error_bps_w=ilc.get_error_direction(curve,p_bp,q_bp,curve_exe,curve_exe_R)
-			###line search on gamma
-			max_error=[]
-			gamma_all=np.linspace(gamma_v_min,gamma_v_max,num=int(1+(gamma_v_max-gamma_v_min)/0.2))
-			print(gamma_all)
-			for gamma_v in gamma_all:
-				p_bp_temp, q_bp_temp=ilc.update_error_direction(curve,p_bp,q_bp,error_bps_v,error_bps_w,gamma_v=gamma_v,gamma_w=0.1)
-				ms = MotionSend(url='http://192.168.55.1:80')
-				curve_js_all_new, avg_curve_js, timestamp_d=average_5_exe(ms,robot,primitives,breakpoints,p_bp_temp,q_bp_temp,s,z,curve,"recorded_data")
-				###calculat data with average curve
-				lam, curve_exe, curve_exe_R, speed=logged_data_analysis(robot,timestamp_d,avg_curve_js)
-				#############################chop extension off##################################
-				lam, curve_exe, curve_exe_R,curve_exe_js, speed, timestamp=ms.chop_extension(curve_exe, curve_exe_R,avg_curve_js, speed, timestamp_d,curve[0,:3],curve[-1,:3])
-				##############################calcualte error########################################
-				error_temp,angle_error_temp=calc_all_error_w_normal(curve_exe,curve[:,:3],curve_exe_R[:,:,-1],curve[:,3:])
-				max_error.append(max(error_temp))
+			if i==0:
+				gamma_v=0.8
+			else:
+				###line search on gamma
+				max_error=[]
+				gamma_all=np.linspace(gamma_v_min,gamma_v_max,num=int(1+(gamma_v_max-gamma_v_min)/0.2))
+				print(gamma_all)
+				for gamma_v in gamma_all:
+					p_bp_temp, q_bp_temp=ilc.update_error_direction(curve,p_bp,q_bp,error_bps_v,error_bps_w,gamma_v=gamma_v,gamma_w=0.1)
+					ms = MotionSend(url='http://192.168.55.1:80')
+					curve_js_all_new, avg_curve_js, timestamp_d=average_5_exe(ms,robot,primitives,breakpoints,p_bp_temp,q_bp_temp,s,z,curve,"recorded_data")
+					###calculat data with average curve
+					lam, curve_exe, curve_exe_R, speed=logged_data_analysis(robot,timestamp_d,avg_curve_js)
+					#############################chop extension off##################################
+					lam, curve_exe, curve_exe_R,curve_exe_js, speed, timestamp=ms.chop_extension(curve_exe, curve_exe_R,avg_curve_js, speed, timestamp_d,curve[0,:3],curve[-1,:3])
+					##############################calcualte error########################################
+					error_temp,angle_error_temp=calc_all_error_w_normal(curve_exe,curve[:,:3],curve_exe_R[:,:,-1],curve[:,3:])
+					max_error.append(max(error_temp))
 
-			
-			print(max_error)
-			min_error_idx=np.argmin(max_error)
-			gamma_v=gamma_all[min_error_idx]
-			if min(max_error)>max(error):
-				gamma_v=0
-				max_grad=True
+				
+				print(max_error)
+				min_error_idx=np.argmin(max_error)
+				gamma_v=gamma_all[min_error_idx]
+				if min(max_error)>max(error):
+					gamma_v=0
+					max_grad=True
 			print("FINAL GAMMA",gamma_v)
 			gamma_v_max=gamma_v
 			p_bp, q_bp=ilc.update_error_direction(curve,p_bp,q_bp,error_bps_v,error_bps_w,gamma_v=gamma_v,gamma_w=0.1)
@@ -211,7 +214,7 @@ def main():
 		p_bp_np=np.array(p_bp_np)      ###np version
 		ax.scatter(p_bp_np[:,0], p_bp_np[:,1], p_bp_np[:,2], c='green',label='breakpoints')
 		plt.title("Iteration "+str(i+1)+": 3D Plot")
-		fig_3d.canvas.manager.window.move(1300,566)
+		fig_3d.canvas.manager.window.move(1111,400)
 		plt.legend()
 		plt.show(block=False)
 		plt.pause(0.1)
