778aa5805348aba5e8eb9aff15e77eb640a25ce0
hehonglu123 <hehonglu123@126.com>
2022-12-13 21:12:19 -0500

    m

--- data/pose_ZYX.py
+++ data/pose_ZYX.py
@@ -8,12 +8,12 @@ from robots_def import *
 
 
 dataset='curve_2/'
-solution_dir='curve_pose_opt2/'
+solution_dir='dual_arm/'
 # solution_dir='curve_pose_opt1/'
 data_dir=dataset+solution_dir
 
 ###reference frame transformation
-curve_pose=np.loadtxt(data_dir+'curve_pose.csv',delimiter=',')
+curve_pose=np.loadtxt(data_dir+'tcp.csv',delimiter=',')
 
 print(curve_pose[:-1,-1])
 print(np.degrees(rotationMatrixToEulerAngles(curve_pose[:3,:3])))
